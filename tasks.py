658e5056e599b3d083da656b0e12754a6a17c876
Ronald Abu Saleh <ronald.abusaleh@gmail.com>
2024-05-03 17:41:19 -0400

    Fix

--- tasks.py
+++ tasks.py
@@ -9,7 +9,7 @@ from RPA.Excel.Files import Files
 def robot_spare_bin_python():
     """Insert the sales data for the week and export it as a PDF"""
     browser.configure(
-        slowmo=300
+        slowmo=100,
     )
     open_the_intranet_website()
     log_in()
@@ -30,16 +30,10 @@ def log_in():
     page.click("button:text('Log in')")
 
 
-def download_excel_file():
-    """Downloads excel file from the given URL"""
-    http = HTTP()
-    http.download(
-        url="https://robotsparebinindustries.com/SalesData.xlsx", overwrite=True)
-
-
-def fill_and_submit_form(sales_rep):
+def fill_and_submit_sales_form(sales_rep):
     """Fills in the sales data and click the 'Submit' button"""
     page = browser.page()
+
     page.fill("#firstname", sales_rep["First Name"])
     page.fill("#lastname", sales_rep["Last Name"])
     page.select_option("#salestarget", str(sales_rep["Sales Target"]))
@@ -47,12 +41,19 @@ def fill_and_submit_form(sales_rep):
     page.click("text=Submit")
 
 
+def download_excel_file():
+    """Downloads excel file from the given URL"""
+    http = HTTP()
+    http.download(
+        url="https://robotsparebinindustries.com/SalesData.xlsx", overwrite=True)
+
+
 def fill_form_with_excel_data():
     """Read data from excel and fill in the sales form"""
     excel = Files()
     excel.open_workbook("SalesData.xlsx")
-    worksheet = excel.read_worksheet("data", header=True)
+    worksheet = excel.read_worksheet_as_table("data", header=True)
     excel.close_workbook()
 
     for row in worksheet:
-        fill_and_submit_form(row)
+        fill_and_submit_sales_form(row)
